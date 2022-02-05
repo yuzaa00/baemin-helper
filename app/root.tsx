bd775b5d2f41d76370ae8dc794f6b3a02cb10206
Youn_Ji <syj9760@gmail.com>
2022-02-05 22:58:15 +0900

    feat: ga 추가

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,6 @@
 import { ToastContainer } from '@dano-inc/design-system';
 import { globalCss } from '@dano-inc/stitches-react';
+import { useLayoutEffect } from 'react';
 import {
   Links,
   LinksFunction,
@@ -83,6 +84,10 @@ const resetStyle = globalCss({
 export default function App() {
   resetStyle();
 
+  useLayoutEffect(() => {
+    (window as any).dataLayer = (window as any).dataLayer || [];
+  });
+
   return (
     <html lang="ko">
       <head>
@@ -90,6 +95,17 @@ export default function App() {
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer', 'G-6KWFTN3DEJ');
+  `,
+          }}
         />
         <script
           async
