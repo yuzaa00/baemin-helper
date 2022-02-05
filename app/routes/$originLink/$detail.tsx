d4fb9e7cb514b43e89bebce55f32003659f02966
Youn_Ji <syj9760@gmail.com>
2022-02-05 20:16:35 +0900

    fix: og title, description 추가

--- app/routes/$originLink/$detail.tsx
+++ app/routes/$originLink/$detail.tsx
@@ -23,9 +23,12 @@ export const meta: MetaFunction = ({
 }: {
   data: { data: SingleMenuData };
 }) => {
+  
   return {
-    title: `${data.data.Food_Nm}`,
+    title: `${data.data.Shop_Nm} - ${data.data.Food_Nm}`,
     description: '여기를 눌러 웹에서 손쉽게 메뉴를 확인해보세요!',
+    'og:title': `${data.data.Food_Nm}`,
+    'og:description': '여기를 눌러 웹에서 손쉽게 메뉴를 확인해보세요!',
   };
 };
 
